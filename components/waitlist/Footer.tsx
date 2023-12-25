c15b10d0eb4ff344b998a2ba2af020465d1e1be8
Benjtalkshow <chinedubenj@gmail.com>
2023-12-25 12:59:22 -0800

    Fix: social icons

--- components/waitlist/Footer.tsx
+++ components/waitlist/Footer.tsx
@@ -6,13 +6,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className=''>
-      <div className="w-full py-10 mb-10 dark:bg-black flex justify-center">
-        <div className="flex justify-between w-[95%] md:w-[85%] items-center dark:text-white">
+      <div className="w-full py-10 px-2 mb-10 dark:bg-black flex justify-center">
+        <div className="flex  justify-between w-[95%] md:w-[85%] items-center dark:text-white">
           <Link href="mailto:info@gridape.com" className='hover:text-[#2E3192] whitespace-nowrap max-[640px]:text-sm'>Mail us at: info@gridape.com</Link>
           <div className='flex gap-2 md:gap-5'>
             {footerSocialLinks.map((social, index) => (
               <Link key={index} href={social.link} target='_blank' rel="noopener noreferrer">
-                {/* <Image src={social.name} width={35} height={35} alt='social icon' className='w-[25px] md:w-[35px] h-[25px] md:h-[35px] transition-transform hover:translate-y-2'/> */}
+                <Image src={social.name} width={35} height={35} alt='social icon' className='w-[25px] md:w-[35px] h-[25px] md:h-[35px] transition-transform hover:translate-y-2'/>
               </Link>
             ))}
           </div>
