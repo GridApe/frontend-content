7f70ec9ceac692568db8c1b1bb5a749af356445c
Collins Ikechukwu <collinschristroa@gmail.com>
2023-12-26 09:05:37 +0100

    Fix: set min-height 100vh

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,11 +4,12 @@ import Link from "next/link"
 
 const notFound = () => {
     return (
-        <div className="min-h-[75vh] text-center relative py-[20px] flex items-center justify-center">
+        <div className="min-h-screen text-center relative py-[20px] flex items-center justify-center">
             <div className="container">
                 <div className="row">
                     <div className="col-xxl-6 col-lg-7 m-auto">
-                        <h1>Oops! You&apos;re on the wrong place.</h1>                        <p className="me-xxl-5 ms-xxl-5 py-5">
+                        <h1>Oops! You&apos;re on the wrong place.</h1>
+                        <p className="me-xxl-5 ms-xxl-5 py-5">
                             Unable to find what you&apos;re looking for? Begin your journey from our Homepage.
                         </p>
                         <Link href="/" className="btn-sm mb-10 bg-[#2E3192] text-white whitespace-nowrap  ">
@@ -19,7 +20,6 @@ const notFound = () => {
                 <Image
                     width={640}
                     height={417}
-
                     src="/svg/ils_06.svg"
                     alt="404 Image"
                     className="m-auto"
