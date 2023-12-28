258f4f16d6bccf590c15f27e0002c7ecf875b68f
Benjtalkshow <chinedubenj@gmail.com>
2023-12-27 23:02:57 -0800

    Revert "Fix: Nextjs export error"
    This reverts commit 9b436e95796f159f1f6cfea2a34a162db0fddb52.

--- lib/image/loader.ts
+++ lib/image/loader.ts
@@ -1,5 +1,7 @@
 'use client'
  
 export default function myImageLoader({ src, width, quality } : any) {
-  return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`
+    const url = window.location.origin;
+    console.log(url); 
+  return `${url}${src}?w=${width}&q=${quality || 75}`
 }
