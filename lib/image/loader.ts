9192b3993d179250861037daf7bfc0bca0439e12
Benjtalkshow <chinedubenj@gmail.com>
2023-12-27 23:32:00 -0800

    Revert: --revert

--- lib/image/loader.ts
+++ lib/image/loader.ts
@@ -1,7 +1,7 @@
-'use client'
+// 'use client'
  
-export default function myImageLoader({ src, width, quality } : any) {
-    const url = window.location.origin;
-    console.log(url); 
-  return `${url}${src}?w=${width}&q=${quality || 75}`
-}
+// export default function myImageLoader({ src, width, quality } : any) {
+//     const url = window.location.href;
+//     console.log(url); 
+//   return `${url}${src}?w=${width}&q=${quality || 75}`
+// }
